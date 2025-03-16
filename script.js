b64cca4673ac04e1e88fd7798d0322ba45fd483d
shaik imran <imran44020@gmail.com>
2025-03-16 16:56:06 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").addEventListener("click", function() {
+       alert("Thanks for visiting imrandivu.com for more details please contact us by +91-9032844020 Whatsapp +91-7997805984!");
+     });
